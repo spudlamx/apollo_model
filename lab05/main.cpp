26922d1c196258b33c2494b4c20c7f6049dee044
Spencer Lamoreaux <lam21016@byui.edu>
2023-10-13 11:54:00 -0600

    Initial Commit

---
+++ lab05/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab05
+//
+//  Created by Spencer Lamoreaux on 10/13/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
